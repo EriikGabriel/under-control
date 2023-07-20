e7b6ebb235b63ce91e9f99971e13b6889dba35a2
tkraide <thiagokraide2002@gmail.com>
2023-07-20 02:00:01 -0300

    creating platforms that fall or move

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -78,6 +78,11 @@ func _physics_process(delta):
 	
 	_set_state()
 	if !is_death: move_and_slide()
+	
+	for platforms in get_slide_collision_count():
+		var collision = get_slide_collision(platforms)
+		if collision.get_collider().has_method("has_collided_with"):
+			collision.get_collider().has_collided_with(collision, self)
 
 func _on_hurtbox_body_entered(body: Node2D) -> void:
 	if raycast_right.is_colliding():
